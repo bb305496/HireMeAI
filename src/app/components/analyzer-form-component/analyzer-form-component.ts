a9d1a697a8bfbce7cd1df2a6638688af6b309dc9
bb305496 <bartekbielak12@onet.pl>
2026-03-22 09:38:01 +0100

    Change detection strategy

--- src/app/components/analyzer-form-component/analyzer-form-component.ts
+++ src/app/components/analyzer-form-component/analyzer-form-component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-analyzer-form-component',
   imports: [],
   templateUrl: './analyzer-form-component.html',
   styleUrl: './analyzer-form-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnalyzerFormComponent {}
--- src/app/components/results-component/results-component.ts
+++ src/app/components/results-component/results-component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-results-component',
   imports: [],
   templateUrl: './results-component.html',
   styleUrl: './results-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResultsComponent {}
