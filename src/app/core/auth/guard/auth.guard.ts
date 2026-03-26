9018166908e963061161ae8b4be8c744afeec9c6
bb305496 <bartekbielak12@onet.pl>
2026-03-26 12:40:02 +0100

    Add modal and toast to un logged users

--- src/app/core/auth/guard/auth.guard.ts
+++ src/app/core/auth/guard/auth.guard.ts
@@ -3,13 +3,22 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { ToastService } from '../../ui/toast/service/toast.service';
+import { Store } from '@ngrx/store';
+import { UiActions } from '../../ui/modal/+state/ui.actions';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
+  const toast = inject(ToastService);
+  const store = inject(Store);
 
   return auth.me().pipe(
     map(() => true),
-    catchError(() => of(router.createUrlTree(['/'])))
+    catchError(() => {
+      store.dispatch(UiActions.openModal({ modal: 'login' }));
+      toast.show('Please log in or create account to continue', 'info');
+      return of(router.createUrlTree(['/']));
+    })
   )
 };
