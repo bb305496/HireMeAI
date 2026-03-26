3ec806fa86893e42fd0cbf149a2112ee72ca5725
bb305496 <bartekbielak12@onet.pl>
2026-03-26 19:34:21 +0100

    Analyzer bugfix

--- src/app/core/analysis/+state/analysis.effects.ts
+++ src/app/core/analysis/+state/analysis.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { AnalysisActions } from './analysis.actions';
-import { AnalysisService} from '../services/analysis.service';
+import { AnalysisService } from '../services/analysis.service';
 
 @Injectable()
 export class AnalysisEffects {
